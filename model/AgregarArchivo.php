167c97c0a324c8527364514d7d9796b501f61314
jose_ramon <slayermx_117@hotmail.com>
2015-02-03 22:56:29 -0600

    Carpeta para archivos

---
+++ model/AgregarArchivo.php
@@ -0,0 +1,48 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+include 'Mysql_connection.php';
+
+$tipo = $_POST["tipo"];
+$nombre = $_POST["nombre"];
+$descripcion = $_POST["descripcion"];
+$idMaestro = $_POST["idMaestro"];
+$idMateria = $_POST["idMateria"];
+$tipoBool = 0;
+$url = "";
+
+if (is_uploaded_file($_FILES['archivo']['tmp_name'])) {
+    $nombreDirectorio = "../archivos/";
+    $nombreFichero = $_FILES['archivo']['name'];
+
+    $nombreCompleto = $nombreDirectorio . $nombreFichero;
+    if (is_file($nombreCompleto)) {
+        $idUnico = time();
+        //$nombreFichero = $idUnico . "-" . $nombreFichero;
+    }
+
+    move_uploaded_file($_FILES['archivo']['tmp_name'], $nombreDirectorio . $nombreFichero);
+    $url = $nombreDirectorio . $nombreFichero;
+} else {
+    echo "No se pudo subir el archivo";
+}
+
+
+
+if ($tipo == "examen") {
+    $tipoBool = 0;
+}
+if ($tipo == "tarea") {
+    $tipoBool = 1;
+}
+$query = "INSERT INTO apuntes (NombreApuntes, ArchivoApuntes, DescripcionApuntes,  Tipo, idMaestro, idMateria)
+VALUES ('$nombre', '$url', '$descripcion', $tipoBool, $idMaestro, $idMateria)";
+
+$result = mysql_query($query, $dbhandle);
+header("Location: ../archivoGuardado.html");
+exit;
+?>
---
+++ model/AgregarEnlace.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+include 'Mysql_connection.php';
+
+$link = $_GET["link"];
+$Materia_idMateria = $_GET["Materia_idMateria"];
+
+$query = "INSERT INTO enlaces (NombreEnlace,Materia_idMateria)
+VALUES ('$link', $Materia_idMateria)";
+
+$result = mysql_query($query, $dbhandle);
+header("Location: ../archivoGuardado.html");
+exit;
+?>
